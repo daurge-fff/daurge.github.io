15234cd5e8eee7c8ae025aa02973d5cb7d56344c
Garold <daurge@mail.ru>
2022-01-07 01:14:00 +0500

    Add files via upload

---
+++ info.js
@@ -0,0 +1,5 @@
+<iframe id="myframe" src="texts.txt"> </iframe>
+function myFunction() {
+	const iframe = document.getElementById('texts.txt"');  
+	// Reload the iframe
+	iframe.contentWindow.location.reload()}
